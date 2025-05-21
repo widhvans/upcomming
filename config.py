f40221e72b733444f930623ea162ea1f2db0427c
widhvans <110589162+widhvans@users.noreply.github.com>
2025-05-21 20:13:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Configuration file for Movie Bot
 
 # Telegram Bot Token (get from BotFather)
-TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+TELEGRAM_TOKEN = "5792496493:AAEdISosRZptqrYa-yNbO8R9zrat-L5TiIQ"
 
 # TMDb API Key (get from https://www.themoviedb.org/)
-TMDB_API_KEY = "YOUR_TMDB_API_KEY"
+TMDB_API_KEY = "5a318417c7f4a722afd9d71df548877b"
 
 # MongoDB URI (e.g., from MongoDB Atlas)
-MONGO_URI = "mongodb+srv://username:password@cluster0.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
